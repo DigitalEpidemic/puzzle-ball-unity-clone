d9325bd777d594d33af9e36da327a12fab24cfa3
Jeff Polasz <jeff_polasz@hotmail.com>
2018-03-24 03:58:40 -0400

    Added varying sound volume based on collision

--- Assets/Scripts/Ball/BallSound.cs
+++ Assets/Scripts/Ball/BallSound.cs
@@ -13,9 +13,15 @@ public class BallSound : MonoBehaviour {
 	private AudioSource ballRollAudio;
 
 	[SerializeField]
-	private AudioClip pickUp;
+	private AudioClip pickUp, wallHit;
 
 	private BallMovement ballMovement;
+
+	private Vector3 velocityLastFrame;
+	private Vector3 collisionNormal;
+	private float xAxisAngle, xFactor;
+	private float yAxisAngle, yFactor;
+	private float zAxisAngle, zFactor;
 
 	void Awake () {
 		myBody = GetComponent<Rigidbody> ();
@@ -24,6 +30,10 @@ public class BallSound : MonoBehaviour {
 
 	void Update () {
 		BallRollSoundController ();
+	}
+
+	void LateUpdate () {
+		velocityLastFrame = myBody.velocity;
 	}
 
 	void BallRollSoundController () {
@@ -39,6 +49,34 @@ public class BallSound : MonoBehaviour {
 	public void PlayPickUpSound () {
 		audioSource.volume = 0.7f;
 		audioSource.PlayOneShot (pickUp);
+	}
+
+	void SetSoundVolumeOnCollision (Collision collision) {
+		// Contacts is an array of contact points
+		// Collision normal is a vector that is used to calculate impulses after the collision
+		collisionNormal = collision.contacts [0].normal;
+
+		// Vector3.Angle returns angle in degrees between from and to
+		xAxisAngle = Vector3.Angle (Vector3.right, collisionNormal);
+		xFactor = (1.0f / 8100f) * xAxisAngle * xAxisAngle + (-1 / 45f) + 1f;
+
+		yAxisAngle = Vector3.Angle (Vector3.up, collisionNormal);
+		yFactor = (1.0f / 8100f) * yAxisAngle * yAxisAngle + (-1 / 45f) + 1f;
+
+		zAxisAngle = Vector3.Angle (Vector3.forward, collisionNormal);
+		zFactor = (1.0f / 8100f) * zAxisAngle * zAxisAngle + (-1 / 45f) + 1f;
+
+		audioSource.volume = (Mathf.Abs (velocityLastFrame.x) * xFactor * 0.001f) +
+			(Mathf.Abs (velocityLastFrame.y) * yFactor * 0.001f) +
+			(Mathf.Abs (velocityLastFrame.z) * zFactor * 0.001f);
+	}
+
+	void OnCollisionEnter (Collision target) {
+		SetSoundVolumeOnCollision (target);
+
+		if (target.gameObject.tag == "Wall") {
+			audioSource.PlayOneShot (wallHit);
+		}
 	}
 
 } // BallSound
