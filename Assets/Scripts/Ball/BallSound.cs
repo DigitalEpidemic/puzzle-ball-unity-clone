faa30c06ebe6cf593fde55491e948d91fc230f6c
Jeff Polasz <jeff_polasz@hotmail.com>
2018-03-24 00:56:56 -0400

    Added ball rolling sound

--- Assets/Scripts/Ball/BallSound.cs
+++ Assets/Scripts/Ball/BallSound.cs
@@ -4,18 +4,36 @@ using UnityEngine;
 
 public class BallSound : MonoBehaviour {
 
+	private Rigidbody myBody;
+
 	[SerializeField]
 	private AudioSource audioSource;
 
 	[SerializeField]
+	private AudioSource ballRollAudio;
+
+	[SerializeField]
 	private AudioClip pickUp;
 
-	void Start () {
-		
+	private BallMovement ballMovement;
+
+	void Awake () {
+		myBody = GetComponent<Rigidbody> ();
+		ballMovement = GetComponent<BallMovement> ();
 	}
 
 	void Update () {
-		
+		BallRollSoundController ();
+	}
+
+	void BallRollSoundController () {
+		if (ballMovement.onFloorTracker > 0 && myBody.velocity.sqrMagnitude > 0) {
+			ballRollAudio.volume = myBody.velocity.sqrMagnitude * 0.0002f;
+			ballRollAudio.pitch = 0.4f + ballRollAudio.volume;
+			ballRollAudio.mute = false;
+		} else {
+			ballRollAudio.mute = true;
+		}
 	}
 
 	public void PlayPickUpSound () {
