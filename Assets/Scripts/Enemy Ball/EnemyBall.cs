038a9d2a5f0487daa4220f62840f1e1e12160bf2
Jeff Polasz <jeff_polasz@hotmail.com>
2018-03-27 21:52:15 -0400

    Finished Level4

---
+++ Assets/Scripts/Enemy Ball/EnemyBall.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBall : MonoBehaviour {
+
+	private Transform ballTarget;
+	private Vector3 ballPositionDirection;
+
+	private bool canAttack, readyToAttack;
+
+	[HideInInspector]
+	public bool stunned;
+
+	private Rigidbody myBody;
+	private RaycastHit ballHit;
+
+	void Awake () {
+		myBody = GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+		CheckIfCanAttack ();
+	}
+
+	void FixedUpdate () {
+		Attack ();
+	}
+
+	void GetBallTarget (Transform target) {
+		ballTarget = target;
+	}
+
+	void CanAttackToggle (bool canAttack) {
+		this.canAttack = canAttack;
+	}
+
+	void CheckIfCanAttack () {
+		if (canAttack && !stunned && (myBody.velocity.sqrMagnitude <= 0.11f)) {
+			ballPositionDirection = ballTarget.position - transform.position;
+
+			if (Physics.Raycast (transform.position, ballPositionDirection, out ballHit, 25)) {
+				if (ballHit.transform.tag == "Ball") {
+					readyToAttack = true;
+				}
+			}
+		}
+	}
+
+	void Attack () {
+		if (readyToAttack) {
+			myBody.AddForce (ballPositionDirection * 200f);
+			readyToAttack = false;
+		}
+	}
+
+} // EnemyBall
---
+++ Assets/Scripts/Enemy Ball/EnemyBallTrigger.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyBallTrigger : MonoBehaviour {
+
+	private Rigidbody myBody;
+
+	[SerializeField]
+	private AudioSource ballRollAudio, audioSource, stunnedAudio;
+
+	[SerializeField]
+	private AudioClip wallHit, stunnedClip;
+
+	private Vector3 velocityLastFrame;
+	private Vector3 collisionNormal;
+	private float xAxisAngle, xFactor;
+	private float yAxisAngle, yFactor;
+	private float zAxisAngle, zFactor;
+
+	private EnemyBall enemyBall;
+	private MeshRenderer myRenderer;
+
+	void Awake () {
+		myBody = GetComponent<Rigidbody> ();
+		enemyBall = GetComponent<EnemyBall> ();
+		myRenderer = GetComponent<MeshRenderer> ();
+	}
+
+	void Update () {
+		BallRollSoundController ();
+	}
+
+	void LateUpdate () {
+		velocityLastFrame = myBody.velocity;
+	}
+
+	void BallRollSoundController () {
+		if (myBody.velocity.sqrMagnitude > 0) {
+			ballRollAudio.volume = myBody.velocity.sqrMagnitude * 0.0002f;
+			ballRollAudio.pitch = 0.4f + ballRollAudio.volume;
+			ballRollAudio.mute = false;
+		} else {
+			ballRollAudio.mute = true;
+		}
+	}
+
+	void SetSoundVolumeOnCollision (Collision collision) {
+		// Contacts is an array of contact points
+		// Collision normal is a vector that is used to calculate impulses after the collision
+		collisionNormal = collision.contacts [0].normal;
+
+		// Vector3.Angle returns angle in degrees between from and to
+		xAxisAngle = Vector3.Angle (Vector3.right, collisionNormal);
+		xFactor = (1.0f / 8100f) * xAxisAngle * xAxisAngle + (-1 / 45f) + 1f;
+
+		yAxisAngle = Vector3.Angle (Vector3.up, collisionNormal);
+		yFactor = (1.0f / 8100f) * yAxisAngle * yAxisAngle + (-1 / 45f) + 1f;
+
+		zAxisAngle = Vector3.Angle (Vector3.forward, collisionNormal);
+		zFactor = (1.0f / 8100f) * zAxisAngle * zAxisAngle + (-1 / 45f) + 1f;
+
+		audioSource.volume = (Mathf.Abs (velocityLastFrame.x) * xFactor * 0.001f) +
+			(Mathf.Abs (velocityLastFrame.y) * yFactor * 0.001f) +
+			(Mathf.Abs (velocityLastFrame.z) * zFactor * 0.001f);
+	}
+
+	IEnumerator BallStunned () {
+		yield return new WaitForSeconds (2f);
+		myRenderer.material.color = new Color (255f, 0f, 227f);
+		enemyBall.stunned = false;
+	}
+
+	void OnCollisionEnter (Collision target) {
+		if (target.gameObject.tag == "Wall") {
+			SetSoundVolumeOnCollision (target);
+
+			if (!enemyBall.stunned && (Mathf.Abs (velocityLastFrame.x) * xFactor +
+				Mathf.Abs (velocityLastFrame.y) * yFactor +
+				Mathf.Abs (velocityLastFrame.z) * zFactor) > 15f) {
+
+				enemyBall.stunned = true;
+				myRenderer.material.color = Color.yellow;
+				stunnedAudio.PlayOneShot (stunnedClip);
+				StartCoroutine (BallStunned ());
+			}
+		}
+
+		if (target.gameObject.tag == "Ball") {
+			SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+		}
+	}
+
+	void OnTriggerEnter (Collider target) {
+		if (target.tag == "Ball") {
+			gameObject.SendMessage ("GetBallTarget", target.transform);
+			gameObject.SendMessage ("CanAttackToggle", true);
+		}
+	}
+
+	void OnTriggerExit (Collider target) {
+		if (target.tag == "Ball") {
+			gameObject.SendMessage ("CanAttackToggle", false);
+		}
+	}
+
+} // EnemyBallTrigger
